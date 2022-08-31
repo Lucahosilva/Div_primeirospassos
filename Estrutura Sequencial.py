ce231b1be5972544caf89d54c836dc11bc7bd939
Lucas Silva <lucashosilva@gmail.com>
2022-08-31 11:09:24 -0300

    FAzendo os primeiros exercicicios de Py

---
+++ Estrutura Sequencial.py
@@ -0,0 +1,10 @@
+print("Hello world!")
+
+a = eval(input("digite um numero "))
+b = eval(input("digite um segundo numero "))
+c = a + b
+
+print("A soma é " + c)
+
+
+
--- arquivo.py
+++
@@ -1,5 +0,0 @@
-print(5+5)
-a = 5
-b = 7
-c = a+b
-print (c)
