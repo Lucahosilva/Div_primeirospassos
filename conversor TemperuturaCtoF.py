2740ac2c2a0a80a150bc0cfb0aab566d45bfdbf8
Lucas Silva <lucashosilva@gmail.com>
2022-08-31 14:10:55 -0300

    Conversor de temperatura F para C 9 exercicio da lista de estruturasequencial

---
+++ conversor TemperuturaCtoF.py
@@ -0,0 +1,5 @@
+#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+f = float(input("Insira a temperatura em ºF:  "))
+c = 5*((f-32)/9)
+print( "a temperatura convertida é ", c)
