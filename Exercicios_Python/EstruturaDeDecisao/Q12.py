0539d9a2a8b84b94b3917331bbe900a860deac62
Lucas Silva <lucashosilva@gmail.com>
2022-09-05 12:04:07 -0300

    exercicios feitos ate o 11 12 WIP

---
+++ Exercicios_Python/EstruturaDeDecisao/Q12.py
@@ -0,0 +1,17 @@
+#Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do Imposto de Renda, que depende do salário bruto (conforme tabela abaixo) e 3% para o Sindicato e que o FGTS corresponde a 11% do Salário Bruto, mas não é descontado (é a empresa que deposita). O Salário Líquido corresponde ao Salário Bruto menos os descontos. O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
+#Desconto do IR:
+#Salário Bruto até 900 (inclusive) - isento
+#Salário Bruto até 1500 (inclusive) - desconto de 5%
+#Salário Bruto até 2500 (inclusive) - desconto de 10%
+
+print("=" * 200)
+valorHora= float(input("Insira o valor da hora: "))
+horas = float(input("horas trabalhadas no mês :"))
+salario = valorHora * horas
+
+sind = 0.03 * salario 
+fgts = 0.11 * salario
+inss = 0.1 * salario 
+
+
+
